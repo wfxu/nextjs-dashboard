a93a341bb5868cadff9ca500832abcd3866f1a6d
wfxu <wenfenxu@qq.com>
2024-05-03 13:59:09 +0800

    fix nav links

--- app/dashboard/nav-links.tsx
+++ app/dashboard/nav-links.tsx
@@ -9,9 +9,17 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
  
-// ...
+const links = [
+  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  {
+    name: 'Invoices',
+    href: '/dashboard/invoices',
+    icon: DocumentDuplicateIcon,
+  },
+  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+];
  
-export default function NavLinks({ links}) {
+export default function NavLinks() {
   const pathname = usePathname();
  
   return (
